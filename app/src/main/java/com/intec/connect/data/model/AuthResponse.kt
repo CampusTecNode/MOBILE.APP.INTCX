9486f285b29db95d97479152af0ea851a217c895
LuisEstebanC <ing2.29capellan@gmail.com>
2024-10-15 19:30:41 -0400

    FixProfile

--- app/src/main/java/com/intec/connect/data/model/AuthResponse.kt
+++ app/src/main/java/com/intec/connect/data/model/AuthResponse.kt
@@ -4,5 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 data class AuthResponse(
     @SerializedName("token") val token: String,
-    @SerializedName("userID") val userID: String
+    @SerializedName("userID") val userID: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("lastname") val lastname: String
 )
--- app/src/main/java/com/intec/connect/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/intec/connect/ui/profile/ProfileFragment.kt
@@ -1,19 +1,25 @@
 package com.intec.connect.ui.profile
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.intec.connect.databinding.FragmentProfileBinding
+import com.intec.connect.utilities.Constants.USER_NAME
+import com.intec.connect.utilities.Constants.USER_LAST_NAME
 
 class ProfileFragment : Fragment() {
 
 private var _binding: FragmentProfileBinding? = null
   private val binding get() = _binding!!
 
+  @SuppressLint("SetTextI18n")
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
@@ -24,10 +30,13 @@ private var _binding: FragmentProfileBinding? = null
 
     _binding = FragmentProfileBinding.inflate(inflater, container, false)
     val root: View = binding.root
-
+    val sharedPrefs = requireContext().getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
+    val userName = sharedPrefs.getString(USER_NAME, "") ?: ""
+    val userLastName = sharedPrefs.getString(USER_LAST_NAME, "") ?: ""
+    val fullName = "$userName $userLastName"
     val textView: TextView = binding.profileName
     profileViewModel.text.observe(viewLifecycleOwner) {
-      textView.text = it
+      textView.text = fullName
     }
     return root
   }
